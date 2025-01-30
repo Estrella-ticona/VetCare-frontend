6c42b324e880db817345fe946d71ae9f0e583cf3
Franco Durand <francoduve.25@gmail.com>
2025-01-30 17:09:41 -0500

    Refactor MainLayout to include Navbar and use Outlet for nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
+import { Route, Routes } from "react-router"
 import { Login } from "./iam/components/Login"
 import { Register } from "./iam/components/Register"
 import { Profile } from "./profile/components/Profile"
-import { Navbar } from "./public/components/Navbar"
 import { MainLayout } from "./public/pages/MainLayout"
-import { Route, Routes } from "react-router";
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route
-        path="/*"
-        element={
-          <MainLayout>
-            <Navbar />
-            <Routes>
-              <Route path="/profile" element={<Profile />} />
-
-            </Routes>
-          </MainLayout>
-        }
-      />
+      <Route element={<MainLayout />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
     </Routes>
   )
 }
--- src/public/pages/MainLayout.tsx
+++ src/public/pages/MainLayout.tsx
@@ -1,7 +1,11 @@
-export function MainLayout({ children }: { children: React.ReactNode }) {
+import { Outlet } from "react-router";
+import { Navbar } from "../components/Navbar";
+
+export function MainLayout() {
     return (
         <div className="min-h-screen bg-[#C3D2E7]-500 flex flex-col items-center justify-center">
-            {children}
+            <Navbar />
+            <Outlet />
         </div>
     );
 }
