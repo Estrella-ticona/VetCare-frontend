ecbb0f1a174b40c072f4a3ab9d382116fa953631
Franco Durand <francoduve.25@gmail.com>
2025-02-06 12:43:59 -0500

    Actualizar la clase Pet para establecer la fecha de registro por defecto si no se proporciona

--- src/clientes/model/pet.ts
+++ src/clientes/model/pet.ts
@@ -5,14 +5,17 @@ export class Pet {
     gender?: string;
     age?: number;
     specie?: string;
-    register?: any;
-
-
+    register?: string;
 
     constructor(data: Partial<Pet> = {}) {
         Object.assign(this, data);
-        /* if (data.register) {
-            this.register = new Date(data.register);
-        } */
+
+        if (!this.register) {
+            const date = new Date();
+            const day = String(date.getDate()).padStart(2, '0');
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const year = date.getFullYear();
+            this.register = `${day}-${month}-${year}`;
+        }
     }
 }
