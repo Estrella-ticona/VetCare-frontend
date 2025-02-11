0f1502914b4a326be8f15a39688d91589bb22eb6
estrella <estrelladelpilarticonapanduro@gmail.com>
2025-02-11 18:33:17 -0500

    add: prueba_no_sirve

--- src/citas/components/FormAddAppointment.tsx
+++ src/citas/components/FormAddAppointment.tsx
@@ -1,16 +1,15 @@
 import { CheckIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import { Button, Dialog } from "@mui/material";
+import Autocomplete from '@mui/material/Autocomplete';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useContext } from "react";
 import { AppointmentContext } from "../contexts/appointment-context";
-
-
 export function FormAddAppointment({ open, handleClose, }: { open: boolean, handleClose: () => void }) {
 
     /* TODO ESTO SE DEBE DE CAMBIAR A PET */
 
-    const { appointment, clearAppointment, createAppointment, handleChangeAppointment, } = useContext(AppointmentContext);
+    const { appointment, clearAppointment, createAppointment, handleChangeAppointment, clients, pets } = useContext(AppointmentContext);
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
@@ -38,6 +37,19 @@ export function FormAddAppointment({ open, handleClose, }: { open: boolean, hand
                             sx={{ '& .MuiTextField-root': { m: 1, width: '35ch' } }}
                             noValidate
                             autoComplete="off">
+                            <Autocomplete
+                                disablePortal
+                                options={clients.map((option) => option.name)}
+                                onChange={(e, value) => alert(value)}
+                                sx={{ width: 300 }}
+                                renderInput={(params) => <TextField {...params} label="Dueño" />}
+                            />
+                            <Autocomplete
+                                disablePortal
+                                options={pets.map((option) => option.name)}
+                                sx={{ width: 300 }}
+                                renderInput={(params) => <TextField {...params} label="Mascotas" />}
+                            />
                             <TextField
                                 required
                                 id="outlined-required"
